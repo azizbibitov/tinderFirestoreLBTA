736f01bed6e371f9437b4233d497b61f8911fc8f
Aziz Bibitov <azizbibitov11@gmail.com>
2023-09-16 13:43:45 +0500

    Tutorial 10

--- TinderFirestoreLBTA/Views/CardView.swift
+++ TinderFirestoreLBTA/Views/CardView.swift
@@ -19,13 +19,21 @@ class CardView: UIView {
     
     // encapsulation
     fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    fileprivate let gradientLayer = CAGradientLayer()
     fileprivate let informationLabel = UILabel()
     
     // Configurations
     fileprivate let threshold: CGFloat = 80
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupLayout()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+    fileprivate func setupLayout() {
         // custom drawing code
         layer.cornerRadius = 10
         clipsToBounds = true
@@ -34,28 +42,43 @@ class CardView: UIView {
         addSubview(imageView)
         imageView.fillSuperview()
         
+        // add a gradient layer somehow
+        setupGradientLayer()
+        
         addSubview(informationLabel)
         informationLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
         
-        informationLabel.text = "TEST NAME TEST NAME AGE"
         informationLabel.textColor = .white
-        informationLabel.font = UIFont.systemFont(ofSize: 34, weight: .heavy)
         informationLabel.numberOfLines = 0
-        
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        addGestureRecognizer(panGesture)
     }
     
-    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
-        switch gesture.state {
-        case .changed:
-            handleChanged(gesture)
-        case .ended:
-            handleEnded(gesture: gesture)
-        default:
-            ()
-        }
+    fileprivate func setupGradientLayer() {
+        // how we can draw a gradient with Swift
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1.1]
+        // self.frame is actually zero frame
+        
+        layer.addSublayer(gradientLayer)
     }
+    
+    override func layoutSubviews() {
+        // in here you know what you CardView frame will be
+        gradientLayer.frame = self.frame
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+           
+           switch gesture.state {
+           case .began:
+               self.superview?.subviews.last?.layer.removeAllAnimations()
+           case .changed:
+               handleChanged(gesture)
+           case .ended:
+               handleEnded(gesture)
+           default:
+               ()
+           }
+       }
     
     fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
@@ -68,25 +91,29 @@ class CardView: UIView {
         self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
     }
     
-    fileprivate func handleEnded(gesture: UIPanGestureRecognizer) {
-        let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
-        let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
-        
-        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-            if shouldDismissCard {
-                self.frame = CGRect(x: 600 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
-            } else {
-                self.transform = .identity
-            }
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
             
-        }) { (_) in
-            self.transform = .identity
-            if shouldDismissCard {
-                self.removeFromSuperview()
+             //set up functionality to dismiss the card from left to right and backwards
+            let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+            let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
+
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+
+                if shouldDismissCard {
+                    self.layer.frame = CGRect(x: 600 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+                    //self.superview?.subviews.last?.layer.removeAllAnimations()
+                } else {
+                    // bringing back to starting point
+                    self.transform = .identity
+                }
+            }) { (_) in
+               self.transform = .identity
+                
+                if shouldDismissCard {
+                    self.removeFromSuperview()
+                }
             }
-//            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
         }
-    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
