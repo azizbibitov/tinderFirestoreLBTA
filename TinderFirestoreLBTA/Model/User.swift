864d7be8342c093074f9bbe1f8c37f29678c65c8
Aziz Bibitov <azizbibitov11@gmail.com>
2023-09-16 13:06:24 +0500

    Tutorial 5

---
+++ TinderFirestoreLBTA/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  TinderFirestoreLBTA
+//
+//  Created by Aziz Bibitov on 16.09.2023.
+//
+
+import Foundation
+
+struct User {
+    // defining our properties for our model layer
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+}
